7289c1946856b3715674842bbbe7cc8173b221cf
Otavio Pereira Martins <opereirm@emeal.nttdata.com>
2025-09-15 22:50:50 -0300

    Finalizando log_reader e criando Dockerfile.

--- log_reader.py
+++ log_reader.py
@@ -1,7 +1,8 @@
 
 log_file = 'log.log'
+log_file_saída = 'log_tratado.log'
 
-with open(log_file, 'r') as file:
+with open(log_file, 'r') as file, open(log_file_saída, 'w') as file_saída:
     for line in file:
         if 'CRITICAL' in line or 'ERROR' in line:
-            print(line.strip())
+            file_saída.write(line)
