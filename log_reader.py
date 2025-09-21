722e2ace0e1c215720201c5822ef1176c81fe8ce
opmartins <opmartins@outlook.com>
2025-09-20 21:21:31 -0300

    Adicionando leitura do rules e argumentos da função.

--- log_reader.py
+++ log_reader.py
@@ -1,8 +1,15 @@
+import sys
+import yaml
 
-log_file = 'log.log'
+log_file = sys.argv[1]
+
+with open("rules.yaml", "r") as rules_file:
+    rules = yaml.safe_load(rules_file)
+patterns = [rule["regex"] for rule in rules]
+
 log_file_saída = 'log_tratado.log'
 
 with open(log_file, 'r') as file, open(log_file_saída, 'w') as file_saída:
     for line in file:
-        if 'CRITICAL' in line or 'ERROR' in line:
+        if any(pattern in line for pattern in patterns):
             file_saída.write(line)
